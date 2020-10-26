cc3d8bd912e3555bc8ded746841ce86a4a356701
Nandor Biro <dev@nandito.info>
2020-10-26 06:49:34 +0100

    chore: set up vercel deployment

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
@@ -14,10 +13,6 @@ module.exports = {
       template: 'index.html'
     }),
   ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
   module: {
     rules: [
       {
