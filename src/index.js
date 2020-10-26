5a8dcef70c62b9d46e06a32f5bd1cc085fcde451
Nandor Biro <dev@nandito.info>
2020-10-26 19:22:33 +0100

    chore: set up linter, apply rules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let front = false;
 let videoTrack = null;
 let requestAnimationFrameId = null;
+let model;
 
 const video = document.getElementById('webcam');
 const liveView = document.getElementById('liveView');
@@ -9,10 +10,9 @@ const enableWebcamButton = document.getElementById('webcamButton');
 
 function debounce(func, wait, immediate) {
   let timeout;
-  return function () {
-    const context = this
-    const args = arguments;
-    const later = function () {
+  return (...args) => {
+    const context = this;
+    const later = () => {
       timeout = null;
       if (!immediate) func.apply(context, args);
     };
@@ -21,59 +21,11 @@ function debounce(func, wait, immediate) {
     timeout = setTimeout(later, wait);
     if (callNow) func.apply(context, args);
   };
-};
-
-// Check if webcam access is supported.
-function getUserMediaSupported() {
-  return !!(navigator.mediaDevices &&
-    navigator.mediaDevices.getUserMedia);
-}
-
-// If webcam supported, add event listener to button for when user
-// wants to activate it to call enableCam function which we will 
-// define in the next step.
-if (getUserMediaSupported()) {
-  enableWebcamButton.addEventListener('click', enableCam);
-} else {
-  console.warn('getUserMedia() is not supported by your browser');
-}
-
-// Placeholder function for next step. Paste over this in the next step.
-function enableCam(event) {
-  // Only continue if the COCO-SSD has finished loading.
-  if (!model) {
-    return;
-  }
-  
-  // Hide the button once clicked.
-  event.target.classList.add('removed');  
-  
-  // getUsermedia parameters to force video but not audio.
-  const constraints = {
-    audio: false,
-    video: {
-      width: { ideal: window.innerWidth, max: 1920 },
-      height: { max: 1080 },
-      facingMode: (front ? "user" : "environment"),
-    },
-  };
-
-  // Activate the webcam stream.
-  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-    const videoTracks = stream.getVideoTracks();
-
-    if (videoTracks.length) {
-      videoTrack = videoTracks[0];
-    }
-
-    video.srcObject = stream;
-    video.addEventListener('loadeddata', predictWebcam);
-  });
 }
 
 const children = [];
 function clearHighlights() {
-  for (let i = 0; i < children.length; i++) {
+  for (let i = 0; i < children.length; i += 1) {
     liveView.removeChild(children[i]);
   }
   children.splice(0);
@@ -82,31 +34,31 @@ function clearHighlights() {
 // Placeholder function for next step.
 function predictWebcam() {
   // Now let's start classifying a frame in the stream.
-  model.detect(video).then(function (predictions) {
+  model.detect(video).then((predictions) => {
     // Remove any highlighting we did previous frame.
     clearHighlights();
-    
+
     // Now lets loop through predictions and draw them to the live view if
     // they have a high confidence score.
-    for (let n = 0; n < predictions.length; n++) {
+    for (let n = 0; n < predictions.length; n += 1) {
       // If we are over 66% sure we are sure we classified it right, draw it!
       if (predictions[n].score > 0.66) {
         // console.log(predictions[n].class, Math.round(parseFloat(predictions[n].score) * 100));
 
         const p = document.createElement('p');
-        p.innerText = predictions[n].class  + ' - with ' 
-            + Math.round(parseFloat(predictions[n].score) * 100) 
-            + '% confidence.';
-        p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
-            + (predictions[n].bbox[1] - 10) + 'px; width: ' 
-            + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
+        p.innerText = `${predictions[n].class} - with ${
+          Math.round(parseFloat(predictions[n].score) * 100)
+        }% confidence.`;
+        p.style = `margin-left: ${predictions[n].bbox[0]}px; margin-top: ${
+          predictions[n].bbox[1] - 10}px; width: ${
+          predictions[n].bbox[2] - 10}px; top: 0; left: 0;`;
 
         const highlighter = document.createElement('div');
         highlighter.setAttribute('class', 'highlighter');
-        highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
-            + predictions[n].bbox[1] + 'px; width: ' 
-            + predictions[n].bbox[2] + 'px; height: '
-            + predictions[n].bbox[3] + 'px;';
+        highlighter.style = `left: ${predictions[n].bbox[0]}px; top: ${
+          predictions[n].bbox[1]}px; width: ${
+          predictions[n].bbox[2]}px; height: ${
+          predictions[n].bbox[3]}px;`;
 
         liveView.appendChild(highlighter);
         liveView.appendChild(p);
@@ -114,37 +66,82 @@ function predictWebcam() {
         children.push(p);
       }
     }
-    
+
     // Call this function again to keep predicting when the browser is ready.
     requestAnimationFrameId = window.requestAnimationFrame(predictWebcam);
   });
 }
 
-// Store the resulting model in the global scope of our app.
-var model = undefined;
+// Placeholder function for next step. Paste over this in the next step.
+function enableCam(event) {
+  // Only continue if the COCO-SSD has finished loading.
+  if (!model) {
+    return;
+  }
+
+  // Hide the button once clicked.
+  event.target.classList.add('removed');
+
+  // getUsermedia parameters to force video but not audio.
+  const constraints = {
+    audio: false,
+    video: {
+      width: { ideal: window.innerWidth, max: 1920 },
+      height: { max: 1080 },
+      facingMode: (front ? 'user' : 'environment'),
+    },
+  };
+
+  // Activate the webcam stream.
+  navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    const videoTracks = stream.getVideoTracks();
+
+    if (videoTracks.length) {
+      [videoTrack] = videoTracks;
+    }
+
+    video.srcObject = stream;
+    video.addEventListener('loadeddata', predictWebcam);
+  });
+}
+
+// Check if webcam access is supported.
+function getUserMediaSupported() {
+  return !!(navigator.mediaDevices
+    && navigator.mediaDevices.getUserMedia);
+}
+
+// If webcam supported, add event listener to button for when user
+// wants to activate it to call enableCam function which we will
+// define in the next step.
+if (getUserMediaSupported()) {
+  enableWebcamButton.addEventListener('click', enableCam);
+} else {
+  console.warn('getUserMedia() is not supported by your browser');
+}
 
 // Before we can use COCO-SSD class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment 
+// loading. Machine Learning models can be large and take a moment
 // to get everything needed to run.
 // Note: cocoSsd is an external object loaded from our index.html
 // script tag import so ignore any warning in Glitch.
-cocoSsd.load().then(function (loadedModel) {
+cocoSsd.load().then((loadedModel) => {
   model = loadedModel;
   // Show demo section now model is ready to use.
   demosSection.classList.remove('invisible');
 });
 
-document.getElementById("flipButton").addEventListener("click", function() {
+document.getElementById('flipButton').addEventListener('click', () => {
   if (videoTrack) {
     front = !front;
 
     videoTrack.applyConstraints({
-      facingMode: (front ? "user" : "environment"),
+      facingMode: (front ? 'user' : 'environment'),
     });
   }
 });
 
-document.getElementById("stopButton").addEventListener("click", function () {
+document.getElementById('stopButton').addEventListener('click', () => {
   if (videoTrack) {
     videoTrack.stop();
   }
@@ -155,15 +152,15 @@ document.getElementById("stopButton").addEventListener("click", function () {
   video.srcObject = null;
   videoTrack = null;
   requestAnimationFrameId = null;
-  enableWebcamButton.classList.remove("removed");
+  enableWebcamButton.classList.remove('removed');
 });
 
-const setVideoConstraints = debounce(function() {
+const setVideoConstraints = debounce(() => {
   console.log('resize');
   if (videoTrack) {
     videoTrack.applyConstraints({
       width: window.innerWidth,
-    })
+    });
   }
 }, 500);
 
