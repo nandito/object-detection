e4c0afb00a76f372182d4cc543ac7d4c5d87d24e
Nandor Biro <dev@nandito.info>
2020-10-26 06:35:26 +0100

    chore: split webpack config to prod and dev

--- webpack.common.js
+++ webpack.common.js
@@ -3,14 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: {
     app: './src/index.js',
     style: './src/styles.css',
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
   },
   plugins: [
     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
