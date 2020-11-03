8d7eaacac759cd312df090554aa255c04078196f
Nandor Biro <dev@nandito.info>
2020-11-03 18:15:46 +0100

    feat: persist same class predictions in different time

--- src/lib/persist.js
+++ src/lib/persist.js
@@ -1,5 +1,10 @@
-function persist(cache, persisted, now = Date()) {
-  const newPredictions = cache.filter((cp) => !persisted.some((pp) => pp.class === cp.class));
+const TIME_FRAME = 60; // 1 min
+
+const diffSecs = (date1, date2) => (date1 - date2) / 1000;
+
+function persist(cache, persisted, now = new Date()) {
+  const newPredictions = cache.filter((cp) => !persisted.some((pp) => pp.class === cp.class)
+    || persisted.some((pp) => pp.class === cp.class && diffSecs(new Date(), pp?.out) > TIME_FRAME));
   const removedPredictions = persisted.filter((cp) => !cache.some((pp) => pp.class === cp.class));
 
   return [...persisted, ...newPredictions].reduce((acc, curr) => [
--- src/lib/persist.test.js
+++ src/lib/persist.test.js
@@ -1,9 +1,11 @@
 import test from 'ava';
 import persist from './persist';
 
-const person = { class: 'person' };
-const dog = { class: 'dog' };
-const now = Date();
+const person = { class: 'person', in: new Date() };
+const dog = { class: 'dog', in: new Date() };
+const now = new Date();
+const someSecAgo = ((d) => new Date(d.setSeconds(d.getSeconds() - 58)))(new Date());
+const yesterday = ((d) => new Date(d.setDate(d.getDate() - 1)))(new Date());
 
 const cases = [
   {
@@ -32,6 +34,35 @@ const cases = [
     cache: [dog],
     persisted: [person, dog],
     output: [{ ...person, out: now }, dog],
+    date: now,
+  },
+  {
+    name: 'has 1 cached, has 1 persisted - same class, different time',
+    cache: [{ ...person }],
+    persisted: [{ ...person, out: yesterday }],
+    output: [{ ...person, out: yesterday }, { ...person }],
+    date: now,
+  },
+  {
+    name: 'has 2 cached, has 2 persisted - same class, different time',
+    cache: [{ ...person }, { ...dog }],
+    persisted: [{ ...person, out: yesterday }, { ...dog, out: yesterday }],
+    output: [
+      { ...person, out: yesterday },
+      { ...dog, out: yesterday },
+      { ...person },
+      { ...dog },
+    ],
+    date: now,
+  },
+  {
+    name: 'has 2 cached, has 2 persisted - same class, different time - in timeframe',
+    cache: [{ ...person }, { ...dog }],
+    persisted: [{ ...person, out: someSecAgo }, { ...dog, out: someSecAgo }],
+    output: [
+      { ...person, out: someSecAgo },
+      { ...dog, out: someSecAgo },
+    ],
     date: now,
   },
 ];
